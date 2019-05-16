3db7618ddede367f8e167fd8a859084b0da8bec6
github username <github email address>
2019-05-16 15:28:19 -0400

    created actor modal

--- src/components/ActorCard.js
+++ src/components/ActorCard.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import { Grid, Image, Segment, Button} from 'semantic-ui-react'
+import ActorModal from '../components/ActorModal'
 
 class ActorCard extends React.Component {
 
+  modalButton = () => {
+    return (
+      <Button
+        color="yellow"
+        content='View Actor Details'
+        icon='info'
+        labelPosition='left'
+      />
+    )
+  }
   render(){
     const { actor } = this.props
-    const sortedMovies = actor.movies.sort((a,b) => a.year <= new Date().getFullYear() ? b.year - a.year : null).slice(0,10)
+    const sortedMovies = actor.movies.sort((a,b) => a.year <= new Date().getFullYear() ? b.year - a.year : null).slice(0,5)
     const formattedMovies = () => sortedMovies.map(movie => <li key={movie.id}>{movie.year}: {movie.title}</li>)
 
     return(
       actor ?
+      <>
       <Segment >
         <div className="actorCard">
           <Grid stackable columns={2}>
@@ -23,17 +35,14 @@ class ActorCard extends React.Component {
                 <h3>Recent Features</h3>
                 <ul>{formattedMovies()}</ul>
                 <br/>
-                <Button
-                  color="yellow"
-                  content='View Actor Details'
-                  icon='info'
-                  labelPosition='left'
-                />
+                <ActorModal actor={actor} button={this.modalButton()}/>
               </div>
             </Grid.Column>
           </Grid>
         </div>
       </Segment>
+
+      </>
       :
       null
     )
---
+++ src/components/ActorModal.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import { Modal, Image, Header, Button, Icon} from 'semantic-ui-react'
+
+class ActorModal extends React.Component {
+
+  state = {
+    open: false
+  }
+
+  open = () => this.setState({ open: true })
+  close = () => this.setState({ open: false })
+  sortedMovies = () => this.props.actor.movies.sort((a,b) => a.year <= new Date().getFullYear() ? b.year - a.year : null).slice(0,20)
+  formattedMovies = () => this.sortedMovies().map(movie => <li key={movie.id}>{movie.year}: {movie.title}</li>)
+
+  openIMDB = () => {
+    return window.open(`https://www.imdb.com/name/${this.props.actor.imdb_id}`, "_blank")
+  }
+
+  nestedModal = () => {
+    return(
+      <Modal
+        open={this.state.open}
+        onOpen={this.open}
+        onClose={this.close}
+        size='large'
+        trigger={
+          <Button color="yellow"  icon>
+            More <Icon name='right chevron' />
+          </Button>
+        }
+      >
+        <Modal.Header>20 Most Recent Features</Modal.Header>
+        <Modal.Content>
+        <ul>{this.formattedMovies()}</ul>
+        </Modal.Content>
+        <Modal.Actions>
+          <Button color="yellow" icon='left chevron' content='Back' onClick={this.close} />
+        </Modal.Actions>
+      </Modal>
+    )
+  }
+
+  render(){
+    const { actor } = this.props
+
+    return(
+      <Modal trigger={this.props.button} size='large' closeIcon>
+        <Modal.Header className="actorNameCaps">{actor.name} </Modal.Header>
+        <Modal.Content image >
+        <Image wrapped size='large' src={actor.img_url} />
+          <Modal.Description>
+            <Header>Born: {actor.birthday}</Header>
+            {actor.deathday ? <h4>Died: ${actor.deathday}</h4> : null}
+            <h3>Biography</h3>
+            <p>{actor.biography}</p>
+          </Modal.Description>
+        </Modal.Content>
+        <Modal.Actions>
+        <Button color="teal" onClick={this.openIMDB} icon>View {actor.name[0].toUpperCase()}{actor.name.split(" ")[0].slice(1)}'s IMDB Profile</Button>
+        {this.nestedModal()}
+        </Modal.Actions>
+      </Modal>
+    )
+  }
+
+}
+
+export default ActorModal
